628f7231d9d8597ffbd585322dd1fdc4a06e2c87
jishaps3@gmail.com <jishaps3@gmail.com>
2024-06-25 12:21:52 +0530

    add express.json

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 const express = require('express')
 const core = require('cors')
-const bodyParser = require('body-parser')
 const app = express();
-app.use(bodyParser())
+
+app.use(express.json());
 app.use(core())
 
 app.use('/api/v1',require('./router/routing'))
