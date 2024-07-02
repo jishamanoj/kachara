90bea9dcbdcc8483e2af622de2413df64095389d
jishaps3@gmail.com <jishaps3@gmail.com>
2024-07-02 14:00:28 +0530

    update router

--- router/routing.js
+++ router/routing.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-app.use(bodyParser())
+app.use(express.json());
 app.use(cors())
 app.use('/user',require('../controller/userController'));
 app.use('/collectors',require('../controller/wastecollectorController'));
